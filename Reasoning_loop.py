c6c81394459d32dd8582620bedb3b4422e852ca8
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-12-02 18:36:53 +0000

    Trying with the Mistral model

--- Reasoning_loop.py
+++ Reasoning_loop.py
@@ -2,69 +2,62 @@ import os
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import Settings
-from llama_index.llms.gemini import Gemini 
-from llama_index.embeddings.gemini import GeminiEmbedding
+from llama_index.llms.mistralai import MistralAI
+from llama_index.embeddings.mistralai import MistralAIEmbedding
 from llama_index.core import SummaryIndex, VectorStoreIndex
 from llama_index.core.vector_stores import MetadataFilters
 from typing import List
 from llama_index.core.vector_stores import FilterCondition
 from llama_index.core.tools import FunctionTool
 from llama_index.core.tools import QueryEngineTool
+from llama_index.core.agent import FunctionCallingAgentWorker
+from llama_index.core.agent import AgentRunner
 
-
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY") 
 
 documents = SimpleDirectoryReader(input_files=["metagpt.pdf"]).load_data()
 splitter = SentenceSplitter(chunk_size=1024)
-nodes = splitter.get_nodes_from_documents(documents) 
+nodes = splitter.get_nodes_from_documents(documents)
 
-Settings.llm = Gemini(
-    model="models/gemini-1.5-flash", api_key=GOOGLE_API_KEY
 
+mistral_api_key = os.environ.get("MISTRAL_API_KEY")
+
+
+Settings.llm = MistralAI(
+    api_key=mistral_api_key,
+    model="mistral-large-latest",  # or "mistral-medium", "mistral-small"
 )
 
 
-
-Settings.embed_model = GeminiEmbedding(
-    model_name="models/embedding-001", api_key=GOOGLE_API_KEY
+Settings.embed_model = MistralAIEmbedding(
+    api_key=mistral_api_key, model="mistral-embed"
 )
-
-
 
 
 vector_index = VectorStoreIndex(nodes, embed_model=Settings.embed_model)
 
 
-def vector_query(
-    query: str, 
-    page_numbers: List[str]) -> str:
+def vector_query(query: str, page_numbers: List[str]) -> str:
     """Perform a vector search over an index.
-    
+
     query (str): the string query to be embedded.
     page_numbers (List[str]): Filter by set of pages. Leave BLANK if we want to perform a vector search
         over all pages. Otherwise, filter by the set of specified pages.
-    
+
     """
 
-    metadata_dicts = [
-        {"key": "page_label", "value": p} for p in page_numbers
-    ]
-    
+    metadata_dicts = [{"key": "page_label", "value": p} for p in page_numbers]
+
     query_engine = vector_index.as_query_engine(
         similarity_top_k=2,
         filters=MetadataFilters.from_dicts(
-            metadata_dicts,
-            condition=FilterCondition.OR
-        )
+            metadata_dicts, condition=FilterCondition.OR
+        ),
     )
     response = query_engine.query(query)
     return response
-    
 
-vector_query_tool = FunctionTool.from_defaults(
-    name="vector_tool",
-    fn=vector_query
-)
+
+vector_query_tool = FunctionTool.from_defaults(name="vector_tool", fn=vector_query)
 
 summary_index = SummaryIndex(nodes)
 summary_query_engine = summary_index.as_query_engine(
@@ -74,16 +67,17 @@ summary_query_engine = summary_index.as_query_engine(
 summary_tool = QueryEngineTool.from_defaults(
     name="summary_tool",
     query_engine=summary_query_engine,
-    description=(
-        "Useful if you want to get a summary of MetaGPT"
-    ),
+    description=("Useful if you want to get a summary of MetaGPT"),
 )
 
-response = Settings.llm.predict_and_call(
-    [vector_query_tool, summary_tool], 
-    "What are the MetaGPT comparisons with ChatDev described on page 8?", 
-    verbose=True
-)
 
+agent_worker = FunctionCallingAgentWorker.from_tools(
+    [vector_query_tool, summary_tool], llm=Settings.llm, verbose=True
+)
+agent = AgentRunner(agent_worker)
+response = agent.query(
+    "Tell me about the agent roles in MetaGPT, "
+    "and then how they communicate with each other."
+)
 
 print(str(response))
